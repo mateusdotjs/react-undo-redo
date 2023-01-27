267b65ebc231962e20444762382a470aabbfac7b
Mateus <mateus.silvainfo@gmail.com>
2023-01-26 21:30:26 -0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,59 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
 function App() {
+
+  const [points, setPoints] = React.useState([])
+  const [undone, setUndone] = React.useState([])
+
+  function handleClick(e) {
+    const obj = {
+      clientY: e.clientY,
+      clientX: e.clientX
+    }
+
+    setPoints((prev) => [...prev, obj])
+  }
+
+  function handleUndoClick(e) {
+    e.stopPropagation()
+    const newArr = points.slice()
+    const lastUndone = newArr.pop()
+    setUndone((prev) => [...prev, lastUndone])
+    setPoints(newArr)
+  }
+
+  function handleRedoClick(e) {
+    e.stopPropagation()
+    const copy = undone.slice()
+    const redo = copy.pop()
+    setUndone(copy)
+    setPoints((prev) => [...prev, redo])
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app" onClick={(e) => handleClick(e)}>
+      <button
+        onClick={(e) => handleUndoClick(e)}
+        disabled={points.length > 0 ? false : true}>
+        Undo
+      </button>
+
+      <button
+        onClick={(e) => handleRedoClick(e)}
+        disabled={undone.length > 0 ? false : true}>
+        Redo
+      </button>
+
+      {
+        points.map((point, index) => {
+          return <div
+            key={index}
+            className='point'
+            style={{ top: point.clientY, left: point.clientX }}>
+          </div>
+        })
+      }
     </div>
   );
 }
